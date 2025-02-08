cd118c12b16c4fa6a992675a5be705d1410c5bab
Oleksandr Liebiediev <woutut@gmail.com>
2025-02-08 21:39:39 +0100

    Add methods to request embeddings.

--- backend/web_app/config.py
+++ backend/web_app/config.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from dataclasses import dataclass
 from functools import lru_cache
 
@@ -6,15 +7,19 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
-class CustomSettings:
-    CUSTOM_SUB_SETTING: str
+class SimilarityProcessorSettings:
+    OPENAI_API_KEY: str
+    OPENAI_EMBEDDINGS_URL: str
+    OPENAI_EMBEDDINGS_MODEL: str
+    OPENAI_EMBEDDINGS_DIMENSIONS: int | None = None
 
 
 @dataclass(frozen=True)
 class Settings:
-    CUSTOM_SETTING: int = 42
-    custom_set_of_settings: CustomSettings = CustomSettings(
-        CUSTOM_SUB_SETTING="sub_setting"
+    similarity_processor: SimilarityProcessorSettings = SimilarityProcessorSettings(
+        OPENAI_API_KEY=os.environ["OPENAI_API_KEY"],
+        OPENAI_EMBEDDINGS_URL="https://api.openai.com/v1/embeddings",
+        OPENAI_EMBEDDINGS_MODEL="text-embedding-3-large",
     )
 
 
---
+++ backend/web_app/tools/similarity_processor/__init__.py
@@ -0,0 +1,72 @@
+import logging
+
+import httpx
+import jmespath
+from tenacity import (
+    RetryCallState,
+    retry,
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_fixed,
+    wait_random,
+)
+
+from web_app.config import get_settings
+from web_app.tools import RetryableException
+from web_app.tools.requester import async_request_api
+
+settings = get_settings()
+
+logger = logging.getLogger(__name__)
+
+
+def failed_get_embeddings(
+    retry_state: RetryCallState,
+) -> None:
+    logger.error(
+        f"Couldn't get search request embedding with "
+        f"{retry_state.attempt_number} attemps ({round(retry_state.idle_for, 2)}s)."
+    )
+    return None
+
+
+@retry(
+    retry=retry_if_exception_type(RetryableException),
+    stop=stop_after_attempt(5),
+    wait=wait_fixed(1) + wait_random(0, 3),
+    retry_error_callback=failed_get_embeddings,
+)
+async def get_embeddings(
+    client: httpx.AsyncClient,
+    embeddings_input: list[str],
+    label: str = "",
+) -> list[list[float]]:
+    input_data: dict[str, str | list[str] | int] = {
+        "input": embeddings_input,
+        "model": settings.similarity_processor.OPENAI_EMBEDDINGS_MODEL,
+    }
+    dimensions = settings.similarity_processor.OPENAI_EMBEDDINGS_DIMENSIONS
+    if dimensions:
+        input_data["dimensions"] = dimensions
+    response = await async_request_api(
+        client=client,
+        url=settings.similarity_processor.OPENAI_EMBEDDINGS_URL,
+        method="POST",
+        json_data=input_data,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {settings.similarity_processor.OPENAI_API_KEY}",
+        },
+        label=f"{label} ({embeddings_input})",
+    )
+    embeddings = jmespath.search("data[].embedding", response)
+    if not embeddings:
+        raise RetryableException(
+            f"Couldn't get embeddings for {label} ({embeddings_input}). Retrying."
+        )
+    if len(embeddings) != len(embeddings_input):
+        raise RetryableException(
+            f"Got {len(embeddings)} embeddings for {len(embeddings_input)} "
+            f"inputs for {label} ({embeddings_input}). Retrying."
+        )
+    return embeddings
