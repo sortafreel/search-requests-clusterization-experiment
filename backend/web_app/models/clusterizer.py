eb83e84e60c775ed625fbe45a22a8dd4d5e385e2
Oleksandr Liebiediev <woutut@gmail.com>
2025-02-09 15:10:01 +0100

    Rename suggestions into phrases.

--- backend/web_app/models/clusterizer.py
+++ backend/web_app/models/clusterizer.py
@@ -1,13 +1,57 @@
 from dataclasses import dataclass
 
+from pydantic import Field, field_validator
+
+from web_app.models import SafeModel
+
 
 @dataclass(frozen=True)
-class SuggestionsGroup:
-    suggestions: list[str]
+class PhrasesGroup:
+    phrases: list[str]
     avg_distance: float | None
 
 
 @dataclass(frozen=True)
-class SuggestionsCluster:
-    suggestions: list[str]
+class PhrasesCluster:
+    phrases: list[str]
     embeddings: list[list[float]]
+
+
+class PhrasesInput(SafeModel):
+    phrases: list[str]
+
+    # noinspection PyNestedDecorators
+    @field_validator("phrases", mode="after")
+    @classmethod
+    def validate_phrases(cls, values: list[str]) -> list[str]:
+        for value in values:
+            if len(value) < 1 or len(value) > 1000:
+                raise ValueError(
+                    f"Only phrases from 1 to 1000 symbols are allowed, "
+                    f'got {len(value)} sumbols: "{value}".'
+                )
+        return values
+
+    @property
+    def unique_phrases(self) -> list[str]:
+        """
+        Make sure to get only unique phrases (strip/lower), to process each one only once
+        :return: Unique phrases, keeping the order.
+        """
+        return list(dict.fromkeys([x.strip().lower() for x in self.phrases]))
+
+    @property
+    def sorted_unique_phrases(self) -> list[str]:
+        """
+        :return: Unique phrases, sorted alphabetically.
+        """
+        return sorted(self.unique_phrases)
+
+
+class GroupingPhrasesInput(PhrasesInput):
+    phrases: list[str] = Field(
+        ...,
+        description="Phrases to group.",
+        min_length=1,
+        max_length=8000,
+    )
--- backend/web_app/tools/clusterizer/__init__.py
+++ backend/web_app/tools/clusterizer/__init__.py
@@ -9,7 +9,7 @@ from sklearn.cluster import KMeans, MiniBatchKMeans
 from tqdm.asyncio import tqdm
 
 from web_app.config import get_settings
-from web_app.models.clusterizer import SuggestionsCluster, SuggestionsGroup
+from web_app.models.clusterizer import PhrasesCluster, PhrasesGroup
 from web_app.tools.clusterizer.utils import cosine_similarity, str_chunks
 from web_app.tools.similarity_processor import get_embeddings
 
@@ -19,25 +19,24 @@ settings = get_settings()
 
 class Clusterizer:
     @staticmethod
-    async def _get_suggestions_chunk_embeddings(
+    async def _get_phrases_chunk_embeddings(
         client: httpx.AsyncClient,
         embeddings_input: list[str],
     ) -> tuple[list[str], list[list[float]]]:
-        suggestions_embeddings = await get_embeddings(
+        phrases_embeddings = await get_embeddings(
             client=client,
             embeddings_input=embeddings_input,
-            settings=settings,
-            label="suggestions embeddings",
+            label="phrases embeddings",
         )
-        return embeddings_input, suggestions_embeddings
+        return embeddings_input, phrases_embeddings
 
     @classmethod
-    async def get_all_suggestions_embeddings(
+    async def get_all_phrases_embeddings(
         cls,
-        suggestions_input: list[str],
+        phrases_input: list[str],
     ) -> tuple[list[str], list[list[float]]]:
         asyncio_tasks = []
-        suggestions = []
+        phrases = []
         embeddings = []
         async with httpx.AsyncClient(
             timeout=httpx.Timeout(
@@ -46,41 +45,41 @@ class Clusterizer:
         ) as client:
             # Chunk embeddings to avoid hitting the API limit
             for chunk in str_chunks(
-                suggestions_input,
+                phrases_input,
                 settings.clusterizer.EMBEDDINGS_CHUNK_SIZE,
             ):
                 asyncio_tasks.append(
-                    cls._get_suggestions_chunk_embeddings(
+                    cls._get_phrases_chunk_embeddings(
                         client=client, embeddings_input=chunk
                     )
                 )
             for st in tqdm(
                 asyncio.as_completed(asyncio_tasks),
                 total=len(asyncio_tasks),
-                desc="Getting suggestions embeddings",
+                desc="Getting phrases embeddings",
             ):
-                suggestions_part, embeddings_part = await st
-                suggestions += suggestions_part
+                phrases_part, embeddings_part = await st
+                phrases += phrases_part
                 embeddings += embeddings_part
-        return suggestions, embeddings
+        return phrases, embeddings
 
     @classmethod
-    def clusterize_suggestions(
+    def clusterize_phrases(
         cls,
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         max_tail_size: int,
-        pre_combined_groups: dict[str, SuggestionsGroup] | None = None,
+        pre_combined_groups: dict[str, PhrasesGroup] | None = None,
         iteration: int = 0,
-    ) -> tuple[dict[str, SuggestionsGroup], SuggestionsCluster]:
+    ) -> tuple[dict[str, PhrasesGroup], PhrasesCluster]:
         """
-        Wrapper for clusterizing suggestions, to allow tracking stats
+        Wrapper for clusterizing phrases, to allow tracking stats
         for clusterization iterations only once, on final iteration.
         """
         # Assuming the input is sorted alphabetically in hope to improve grouping quality
         # TODO Sort it when processing the input?
-        return cls._clusterize_suggestions(
-            embedded_suggestions=embedded_suggestions,
+        return cls._clusterize_phrases(
+            embedded_phrases=embedded_phrases,
             embeddings=embeddings,
             max_tail_size=max_tail_size,
             pre_combined_groups=pre_combined_groups,
@@ -89,8 +88,8 @@ class Clusterizer:
 
     @staticmethod
     def sort_relevant_groups(
-        relevant_groups: dict[str, SuggestionsGroup],
-    ) -> dict[str, SuggestionsGroup]:
+        relevant_groups: dict[str, PhrasesGroup],
+    ) -> dict[str, PhrasesGroup]:
         # Sort relevant groups by the average distance, keep the groups with the best distance first
         return dict(
             sorted(  # type: ignore
@@ -101,28 +100,28 @@ class Clusterizer:
         )
 
     @classmethod
-    def _clusterize_suggestions_iteration(
+    def _clusterize_phrases_iteration(
         cls,
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         iteration: int,
         clustering_distance: float,
         clustering_iterations: int,
-    ) -> tuple[list[dict[str, SuggestionsGroup]], list[SuggestionsCluster]]:
-        # Split suggestions into smaller groups based on embeddings
+    ) -> tuple[list[dict[str, PhrasesGroup]], list[PhrasesCluster]]:
+        # Split phrases into smaller groups based on embeddings
         n_clusters = math.ceil(
             len(embeddings) / settings.clusterizer.EMBEDDINGS_CLUSTERING_CHUNK_SIZE
         )
         if n_clusters == 1:
             # If it's a single cluster - create it manually
             init_embeddings_clusters = {
-                "single_cluster": SuggestionsCluster(
-                    suggestions=embedded_suggestions, embeddings=embeddings
+                "single_cluster": PhrasesCluster(
+                    phrases=embedded_phrases, embeddings=embeddings
                 )
             }
         else:
             init_embeddings_clusters = cls._calculate_embeddings_clusters(
-                embedded_suggestions=embedded_suggestions,
+                embedded_phrases=embedded_phrases,
                 embeddings=embeddings,
                 n_clusters=n_clusters,
                 minibatch=True,
@@ -137,14 +136,14 @@ class Clusterizer:
     @classmethod
     def _group_multiple_embeddings_clusters(
         cls,
-        init_embeddings_clusters: dict[str, SuggestionsCluster],
+        init_embeddings_clusters: dict[str, PhrasesCluster],
         iteration: int,
         clustering_distance: float,
         clustering_iterations: int,
-    ) -> tuple[list[dict[str, SuggestionsGroup]], list[SuggestionsCluster]]:
+    ) -> tuple[list[dict[str, PhrasesGroup]], list[PhrasesCluster]]:
         groups = []
         singles = []
-        # Find groups of suggestions in each cluster, one by one
+        # Find groups of phrases in each cluster, one by one
         for i, (_, cluster) in enumerate(
             tqdm(
                 init_embeddings_clusters.items(),
@@ -152,7 +151,7 @@ class Clusterizer:
             )
         ):
             cluster_groups, cluster_singles = cls._group_embeddings_cluster(
-                embedded_suggestions=cluster.suggestions,
+                embedded_phrases=cluster.phrases,
                 embeddings=cluster.embeddings,
                 clustering_distance=clustering_distance,
                 clustering_iterations=clustering_iterations,
@@ -162,45 +161,45 @@ class Clusterizer:
         return groups, singles
 
     @classmethod
-    def _clusterize_suggestions(
+    def _clusterize_phrases(
         cls,
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         max_tail_size: int,
-        pre_combined_groups: dict[str, SuggestionsGroup] | None,
+        pre_combined_groups: dict[str, PhrasesGroup] | None,
         iteration: int,
         clustering_distance: float = settings.clusterizer.EMBEDDINGS_CLUSTERING_DISTANCE,
         clustering_iterations: int = settings.clusterizer.EMBEDDINGS_CLUSTERING_ITERATIONS,
-    ) -> tuple[dict[str, SuggestionsGroup], SuggestionsCluster]:
-        groups, singles = cls._clusterize_suggestions_iteration(
-            embedded_suggestions=embedded_suggestions,
+    ) -> tuple[dict[str, PhrasesGroup], PhrasesCluster]:
+        groups, singles = cls._clusterize_phrases_iteration(
+            embedded_phrases=embedded_phrases,
             embeddings=embeddings,
             iteration=iteration,
             clustering_distance=clustering_distance,
             clustering_iterations=clustering_iterations,
         )
-        combined_groups: dict[str, SuggestionsGroup] = {}
+        combined_groups: dict[str, PhrasesGroup] = {}
         # If pre-combined groups are provided - add them to the combined groups
         if pre_combined_groups:
             groups = [pre_combined_groups] + groups
         for group_set in groups:
             combined_groups = {**combined_groups, **group_set}
         # Combine the singles in the expected format
-        combined_singles = SuggestionsCluster(suggestions=[], embeddings=[])
+        combined_singles = PhrasesCluster(phrases=[], embeddings=[])
         for single in singles:
-            combined_singles.suggestions.extend(single.suggestions)
+            combined_singles.phrases.extend(single.phrases)
             combined_singles.embeddings.extend(single.embeddings)
         # If there are still iterations left - iterate again
         if iteration < settings.clusterizer.EMBEDDINGS_CLUSTERING_MAX_RECURSION:
-            return cls._clusterize_suggestions(
-                embedded_suggestions=combined_singles.suggestions,
+            return cls._clusterize_phrases(
+                embedded_phrases=combined_singles.phrases,
                 embeddings=combined_singles.embeddings,
                 max_tail_size=max_tail_size,
                 pre_combined_groups=combined_groups,
                 iteration=iteration + 1,
             )
         # If the iterations exhausted and the tail is acceptable - return the results
-        if len(combined_singles.suggestions) <= max_tail_size:
+        if len(combined_singles.phrases) <= max_tail_size:
             return combined_groups, combined_singles
         # If the tail is still too large, but no max tail recursions left - return the results anyway
         if iteration >= (
@@ -222,8 +221,8 @@ class Clusterizer:
             ),
             2,
         )
-        return cls._clusterize_suggestions(
-            embedded_suggestions=combined_singles.suggestions,
+        return cls._clusterize_phrases(
+            embedded_phrases=combined_singles.phrases,
             embeddings=combined_singles.embeddings,
             max_tail_size=max_tail_size,
             pre_combined_groups=combined_groups,
@@ -235,23 +234,21 @@ class Clusterizer:
     @classmethod
     def _group_embeddings_cluster(
         cls,
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         clustering_distance: float,
         clustering_iterations: int,
-    ) -> tuple[dict[str, SuggestionsGroup], SuggestionsCluster]:
+    ) -> tuple[dict[str, PhrasesGroup], PhrasesCluster]:
         # Define result variables to update with each iteration
-        result_relevant_groups: dict[str, SuggestionsGroup] = {}
-        result_singles: SuggestionsCluster = SuggestionsCluster(
-            suggestions=[], embeddings=[]
-        )
-        suggestions_input, embeddings_input = embedded_suggestions, embeddings
-        # An expected average of suggestions per group.
+        result_relevant_groups: dict[str, PhrasesGroup] = {}
+        result_singles: PhrasesCluster = PhrasesCluster(phrases=[], embeddings=[])
+        phrases_input, embeddings_input = embedded_phrases, embeddings
+        # An expected average of phrases per group.
         embeddings_per_group = settings.clusterizer.SUGGESTIONS_PER_EMBEDDINGS_GROUP
         # How many times to clusterize until to stop (to disallow while loop to run forever)
         # Decrease the required distance (- quality) and decrease the cluster size (+ quality) with each iteration
         for distance_iteration in range(clustering_iterations):
-            n_clusters = math.ceil(len(suggestions_input) / embeddings_per_group)
+            n_clusters = math.ceil(len(phrases_input) / embeddings_per_group)
             # Decrease required distance to group embeddings with each iteration,
             # to allow more ideas to be grouped and improve the user experience
             avg_distance_threshold = round(
@@ -266,22 +263,22 @@ class Clusterizer:
                 relevant_groups,
                 result_singles,
             ) = cls._group_embeddings_cluster_iteration(
-                embedded_suggestions=suggestions_input,
+                embedded_phrases=phrases_input,
                 embeddings=embeddings_input,
                 n_clusters=n_clusters,
                 avg_distance_threshold=avg_distance_threshold,
             )
-            # Save successfully groupped suggestions
+            # Save successfully groupped phrases
             result_relevant_groups = {**result_relevant_groups, **relevant_groups}
             # If no singles left - nothing to group again, return results
-            if not result_singles.suggestions:
+            if not result_singles.phrases:
                 return result_relevant_groups, result_singles
             # If singles left, but less than a single group - don't group them again
-            if len(result_singles.suggestions) < embeddings_per_group:
+            if len(result_singles.phrases) < embeddings_per_group:
                 return result_relevant_groups, result_singles
             # If enough singles left - try to clusterize them again
-            suggestions_input, embeddings_input = (
-                result_singles.suggestions,
+            phrases_input, embeddings_input = (
+                result_singles.phrases,
                 result_singles.embeddings,
             )
         # Return the final results
@@ -289,11 +286,11 @@ class Clusterizer:
 
     @staticmethod
     def _calculate_embeddings_clusters(
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         n_clusters: int,
         minibatch: bool,
-    ) -> dict[str, SuggestionsCluster]:
+    ) -> dict[str, PhrasesCluster]:
         matrix = np.vstack(embeddings)
         if not minibatch:
             kmeans = KMeans(
@@ -305,77 +302,77 @@ class Clusterizer:
             )
         kmeans.fit_predict(matrix)
         labels: list[int] = kmeans.labels_
-        # Organize clustered suggestions
-        grouped_suggestions: dict[str, SuggestionsCluster] = {}
+        # Organize clustered phrases
+        grouped_phrases: dict[str, PhrasesCluster] = {}
         # Generate unique label for each clustering calculation
         unique_label = str(shortuuid.ShortUUID().random(length=8))
-        for suggestion, label, emb in zip(embedded_suggestions, labels, embeddings):
+        for suggestion, label, emb in zip(embedded_phrases, labels, embeddings):
             formatted_label = f"{label}_{unique_label}"
-            if formatted_label not in grouped_suggestions:
-                grouped_suggestions[formatted_label] = SuggestionsCluster(
-                    suggestions=[], embeddings=[]
+            if formatted_label not in grouped_phrases:
+                grouped_phrases[formatted_label] = PhrasesCluster(
+                    phrases=[], embeddings=[]
                 )
-            grouped_suggestions[formatted_label].suggestions.append(suggestion)
-            grouped_suggestions[formatted_label].embeddings.append(emb)
-        return grouped_suggestions
+            grouped_phrases[formatted_label].phrases.append(suggestion)
+            grouped_phrases[formatted_label].embeddings.append(emb)
+        return grouped_phrases
 
     @classmethod
     def _group_embeddings_cluster_iteration(
         cls,
-        embedded_suggestions: list[str],
+        embedded_phrases: list[str],
         embeddings: list[list[float]],
         n_clusters: int,
         avg_distance_threshold: float | None,
-    ) -> tuple[dict[str, SuggestionsGroup], SuggestionsCluster]:
+    ) -> tuple[dict[str, PhrasesGroup], PhrasesCluster]:
         embeddings_clusters = cls._calculate_embeddings_clusters(
-            embedded_suggestions=embedded_suggestions,
+            embedded_phrases=embedded_phrases,
             embeddings=embeddings,
             n_clusters=n_clusters,
             minibatch=False,
         )
         # Split into relevant groups and singles
-        relevant_groups: dict[str, SuggestionsGroup] = {}
-        singles = SuggestionsCluster(suggestions=[], embeddings=[])
+        relevant_groups: dict[str, PhrasesGroup] = {}
+        singles = PhrasesCluster(phrases=[], embeddings=[])
         for group_label, cluster in embeddings_clusters.items():
-            if len(cluster.suggestions) <= 1:
+            if len(cluster.phrases) <= 1:
                 # Groups with a single idea move to singles automatically
-                singles.suggestions.extend(cluster.suggestions)
+                singles.phrases.extend(cluster.phrases)
                 singles.embeddings.extend(cluster.embeddings)
                 continue
             # If avg distance threshold not provided (init chunking) - don't filter groups
             if not avg_distance_threshold:
                 # Keep the proper-sized groups that are close to each other
-                relevant_groups[group_label] = SuggestionsGroup(
-                    suggestions=cluster.suggestions,
+                relevant_groups[group_label] = PhrasesGroup(
+                    phrases=cluster.phrases,
                     avg_distance=None,
                 )
                 continue
-            # Calculate average distance between all suggestions in the group
+            # Calculate average distance between all phrases in the group
             distances = []
-            for suggestion, emb in zip(cluster.suggestions, cluster.embeddings):
-                for l_suggestion, l_emb in zip(cluster.suggestions, cluster.embeddings):
+            for suggestion, emb in zip(cluster.phrases, cluster.embeddings):
+                for l_suggestion, l_emb in zip(cluster.phrases, cluster.embeddings):
                     if suggestion != l_suggestion:
                         distances.append(cosine_similarity(emb, l_emb))
             # Round to 2 symbols after the dot
             avg_distance = round(sum(distances) / len(distances), 2)
             # Groups that aren't close enough move to singles
             if avg_distance < avg_distance_threshold:
-                singles.suggestions.extend(cluster.suggestions)
+                singles.phrases.extend(cluster.phrases)
                 singles.embeddings.extend(cluster.embeddings)
                 continue
             # Avoid having large loosely-connected groups
             if (
-                len(cluster.suggestions)
+                len(cluster.phrases)
                 > settings.clusterizer.MAX_SUGGESTIONS_PER_EMBEDDINGS_GROUP
             ):
-                singles.suggestions.extend(cluster.suggestions)
+                singles.phrases.extend(cluster.phrases)
                 singles.embeddings.extend(cluster.embeddings)
                 continue
             # Keep the proper-sized groups that are close to each other
-            relevant_groups[group_label] = SuggestionsGroup(
+            relevant_groups[group_label] = PhrasesGroup(
                 # Don't save embeddings, as the group is already relevant,
                 # so won't go through the clusterization again
-                suggestions=cluster.suggestions,
+                phrases=cluster.phrases,
                 avg_distance=avg_distance,
             )
         return relevant_groups, singles
