c3cedd18a88470e694ec66b758565cd439a109c2
Oleksandr Liebiediev <woutut@gmail.com>
2025-02-13 22:22:37 +0100

    Ensure unique text.

--- backend/web_app/models/embeddings.py
+++ backend/web_app/models/embeddings.py
@@ -5,7 +5,7 @@ from web_app.models.base import SafeModel
 
 class Embedding(SQLModel, table=True):
     id: int = Field(primary_key=True, nullable=False)
-    text: str = Field(index=True, nullable=False)
+    text: str = Field(index=True, nullable=False, unique=True)
 
 
 class EmbeddingCreate(SafeModel):
