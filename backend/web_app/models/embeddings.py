5cc415f64664da919a404361e980d270a11a17ef
Oleksandr Liebiediev <woutut@gmail.com>
2025-02-13 22:25:09 +0100

    Add sample embedding field.

--- backend/web_app/models/embeddings.py
+++ backend/web_app/models/embeddings.py
@@ -6,6 +6,8 @@ from web_app.models.base import SafeModel
 class Embedding(SQLModel, table=True):
     id: int = Field(primary_key=True, nullable=False)
     text: str = Field(index=True, nullable=False, unique=True)
+    # Sample embedding field, later replace with pgvector field
+    embedding: str = Field(nullable=False)
 
 
 class EmbeddingCreate(SafeModel):
